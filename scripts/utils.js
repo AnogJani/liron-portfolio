31c909b0ad4985f5dc56b4365791a3239c2e2c03
AnogJani <anog054@gmail.com>
2024-05-08 12:18:55 +0300

    more changes to mobile function

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,7 +1,19 @@
-const isMobile = navigator.userAgentData.mobile;
 
-console.log(isMobile);
 
-if (isMobile) {
-    document.querySelector("body").style.background = "red";
+function isMobile() {
+    if ('maxTouchPoints' in navigator) return navigator.maxTouchPoints > 0;
+
+    const mQ = matchMedia?.('(pointer:coarse)');
+    if (mQ?.media === '(pointer:coarse)') return !!mQ.matches;
+    
+    if ('orientation' in window) return true;
+    
+    return /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(navigator.userAgent) ||
+      /\b(Android|Windows Phone|iPad|iPod)\b/i.test(navigator.userAgent);
+}
+
+if (isMobile()) {
+    alert("phone");
+} else {
+    alert("desktop");
 }
